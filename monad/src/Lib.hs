0ad9d45137736b64652b939f7a5bd5d31160d7aa
Arthur Caillau <arthur.caillau@airbnb.com>
2016-10-18 22:20:36 -0700

    monad: finish monad chapter
    - Added quickcheck tests
      - Functor
      - Applicative
      - Monad
      - Monoid
    - Implemented the following datatypes
      - Unit
      - Maybe
      - Either
      - Identity
      - List
    - Implemented the following monad functions
      - join
      - liftM
      - liftM2
      - sequenceM
      - others...

--- monad/src/Lib.hs
+++ monad/src/Lib.hs
@@ -19,3 +19,97 @@ instance Monad Nope where
 data PhhhbbtttEither b a
   = L a
   | R b
+  deriving (Eq, Show)
+
+instance Functor (PhhhbbtttEither a) where
+  fmap f (L x) = L $ f x
+  fmap _ (R x) = R x
+
+instance Applicative (PhhhbbtttEither a) where
+  pure = L
+  (R x) <*> _ = R x
+  (L _) <*> (R x) = R x
+  (L f) <*> (L x) = L $ f x
+
+instance Monad (PhhhbbtttEither a) where
+  return = pure
+  (R x) >>= _ = R x
+  (L x) >>= f = f x
+
+newtype Identity a =
+  Identity a
+  deriving (Eq, Ord, Show)
+
+instance Functor Identity where
+  fmap  f (Identity a) = Identity $ f a
+
+instance Applicative Identity where
+  pure = Identity
+  (Identity f) <*> (Identity x) = Identity $ f x
+
+instance Monad Identity where
+  return = pure
+  (Identity x) >>= f = f x
+
+data List a =
+    Nil
+  | Cons a (List a)
+  deriving (Eq, Show)
+
+append :: List a -> List a -> List a
+append Nil ys = ys
+append (Cons x xs) ys = Cons x $ xs `append` ys
+
+fold :: (a -> b -> b) -> b -> List a -> b
+fold _ b Nil = b
+fold f b (Cons x xs) = f x (fold f b xs)
+
+concat' :: List (List a) -> List a
+concat' = fold append Nil
+
+flatMap :: (a -> List b) -> List a -> List b
+flatMap f xs = concat' $ fmap f xs
+
+instance Functor List where
+  fmap _ Nil = Nil
+  fmap f (Cons x xs) = Cons (f x) (fmap f xs)
+
+instance Applicative List where
+  pure x = Cons x Nil
+  Nil <*> _ = Nil
+  fs <*> xs = flatMap (`fmap` xs) fs
+
+instance Monoid (List a) where
+  mempty = Nil
+  mappend = append
+
+instance Monad List where
+  return = pure
+  xs >>= f = flatMap f xs
+
+-- Monad functions
+
+j :: Monad m => m (m a) -> m a
+j mma = mma >>=  id
+
+l1 :: Monad m => (a -> b) -> m a -> m b
+l1 = fmap
+
+l2 :: Monad m => (a -> b -> c) -> m a -> m b -> m c
+l2 f mx my = do
+  x <- mx
+  y <- my
+  return $ f x y
+
+a :: Monad m => m a -> m (a -> b) -> m b
+a mx mf = mf >>= \f -> l1 f mx
+
+meh :: Monad m => [a] -> (a -> m b) -> m [b]
+meh [] f = return []
+meh (x:xs) f = do
+  y <- f x
+  ys <- meh xs f
+  return (y:ys)
+
+flipType :: (Monad m) => [m a] -> m [a]
+flipType xms = meh xms id
--- monad/test/Spec.hs
+++ monad/test/Spec.hs
@@ -1,20 +1,58 @@
 import Data.Monoid
 import Test.QuickCheck (Arbitrary, arbitrary, frequency)
-import Test.QuickCheck.Checkers (quickBatch, EqProp, (=-=), eq)
+import Test.QuickCheck.Checkers (verboseBatch, quickBatch, EqProp, (=-=), eq)
 import Test.QuickCheck.Classes (monoid, applicative, functor, monad)
-import Lib (Nope(..))
+import Lib (Nope(..), PhhhbbtttEither(..), Identity(..), List(..))
 
 instance Eq a => EqProp (Nope a) where (=-=) = eq
 
+instance (Eq a, Eq b) => EqProp (PhhhbbtttEither a b) where (=-=) = eq
+
+instance Eq a => EqProp (Identity a) where (=-=) = eq
+
+instance Eq a => EqProp (List a) where (=-=) = eq
+
 instance Arbitrary (Nope a) where
   arbitrary = return NopeDotJpg
+
+instance (Arbitrary a, Arbitrary b) => Arbitrary (PhhhbbtttEither a b) where
+  arbitrary = do
+    x <- arbitrary
+    y <- arbitrary
+    frequency [ (1, return $ L x)
+              , (1, return $ R y)
+              ]
+
+instance Arbitrary a => Arbitrary (Identity a) where
+  arbitrary = fmap Identity arbitrary
+
+instance Arbitrary a => Arbitrary (List a) where
+  arbitrary = do
+    x <- arbitrary
+    xs <- arbitrary
+    frequency [ (1, return Nil)
+              , (1, return (Cons x xs))
+              ]
 
 main :: IO ()
 main = do
   putStrLn "\nFunctor:"
   quickBatch $ functor (undefined :: Nope (String, String, Integer))
+  quickBatch $ functor (undefined :: PhhhbbtttEither String (String, String, Integer))
+  quickBatch $ functor (undefined :: Identity (String, String, Integer))
+  quickBatch $ functor (undefined :: List (String, String, Integer))
 
   putStrLn "\nApplicative:"
   quickBatch $ applicative (undefined :: Nope (String, String, Integer))
+  quickBatch $ applicative (undefined :: PhhhbbtttEither String (String, String, Integer))
+  quickBatch $ applicative (undefined :: Identity (String, String, Integer))
+  quickBatch $ applicative (undefined :: List (String, String, Integer))
+
   putStrLn "\nMonad:"
   quickBatch $ monad (undefined :: Nope (String, String, Integer))
+  quickBatch $ monad (undefined :: PhhhbbtttEither String (String, String, Integer))
+  quickBatch $ monad (undefined :: Identity (String, String, Integer))
+  quickBatch $ monad (undefined :: List (String, String, Integer))
+
+  putStrLn "\nmonoid:"
+  quickBatch $ monoid (undefined :: List (String, String, Integer))
