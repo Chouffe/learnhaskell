0021fa5673093b2114390f55a1c47d3fffc5a7d7
Arthur Caillau <arthur.caillau@airbnb.com>
2016-12-07 17:32:39 -0800

    [morra] add a 3-gram policy for the AI

--- morragame/src/Lib.hs
+++ morragame/src/Lib.hs
@@ -6,7 +6,8 @@ import Control.Monad.Trans.Class
 import Control.Monad.Trans.Reader
 import Control.Monad.Trans.State
 import Data.Maybe (fromMaybe)
-import Data.List (unlines)
+import Data.List (unlines, zipWith4)
+import qualified Data.Map as M
 import System.Random
 
 -- ADTs and models
@@ -16,16 +17,39 @@ data Mode = Odds | Evens deriving (Eq, Show)
 data Player = Computer Mode | Human Mode deriving (Eq, Show)
 type Fingers = Integer
 
+type Memory = M.Map (Shoot, Shoot, Shoot) Shoot
+type Shoot = Integer
+
+data GameState = GameState { shootSequence :: [Shoot]
+                           , score :: Score
+                           } deriving (Eq, Show)
+
 instance Monoid Score where
   mempty = Score 0 0
   mappend (Score x1 y1) (Score x2 y2) = Score (x1 + x2) (y1 + y2)
 
-check :: Integer -> Either String Integer
+-- TODO: monoid instance for GameState
+
+memory :: [Shoot] -> Memory
+memory [] = M.empty
+memory [x] = M.empty
+memory [x, y] = M.empty
+memory [x, y, z] = M.empty
+memory xs@(x:y:z:u:rs) = M.fromList $ zipWith4 (\a b c d -> ((a, b, c), d)) xs (y:z:u:rs) (z:u:rs) (u:rs)
+
+nextShoot :: [Shoot] -> Maybe Shoot
+nextShoot xs
+  | n <= 3 = Nothing
+  | otherwise = M.lookup (tuplify3 (drop (n - 3) xs)) memo
+  where n = length xs
+        memo = memory xs
+        tuplify3 [x, y, z] = (x, y, z)
+
+check :: Integer -> Either String Shoot
 check x
-  | x > 2 || x <= 0 = Left "Value should be 0 < x <= 2"
+  | x > 2 || x <= 0 = Left "Shoot value should be 0 < x <= 2"
   | otherwise = Right x
 
--- TODO: deal with errors in an either type
 winner :: (Player, Fingers)
        -> (Player, Fingers)
        -> Score
@@ -57,18 +81,28 @@ winnerShow ((Human Odds), h) ((Human Evens), c) =
   else "Player Human 2 wins"
 winnerShow _ _ = undefined -- TODO
 
-drawRandom :: RandomGen g => g -> IO (Integer, g)
+-- TODO: do we need to pass around the random generators?
+drawRandom :: RandomGen g => g -> IO (Shoot, g)
 drawRandom g = randomRIO (0, 1) >>= \x -> return $ (mod x 2, g)
 
--- TODO: add error handling and retries
+drawMemoryOrRandom :: RandomGen g => Mode -> g -> [Shoot] -> IO (Shoot, g)
+drawMemoryOrRandom mode g xs =
+  case (nextShoot xs, mode) of
+    (Just 0, Odds) -> return (1, g)
+    (Just 1, Odds) -> return (0, g)
+    (Just 0, Evens) -> return (0, g)
+    (Just 1, Evens) -> return (1, g)
+    _ -> drawRandom g
+
+
 getLineInteger :: IO Integer
 getLineInteger = do
   l <- getLine
-  case (reads l :: [(Integer, String)]) of
+  case (reads l :: [(Shoot, String)]) of
     [] -> putStrLn "Please enter an Integer.." >> getLineInteger
     [(x, _)] -> return x
 
-drawHuman :: IO Integer
+drawHuman :: IO Shoot
 drawHuman = do
   x <- getLineInteger
   case check x of
@@ -88,12 +122,22 @@ intro players = let labelPlayer   = "-- P is Player"
                  in unlines (labelPlayer:labelComputer:intros)
 
 
-playerShoot :: RandomGen g => g -> Player -> IO (Integer, g)
+playerShoot :: RandomGen g => g -> Player -> IO (Shoot, g)
 playerShoot g (Computer _) = do
   (x, g1) <- drawRandom g
   putStrLn ("C: " ++ show x)
   return (x, g1)
 playerShoot g (Human _) = do
+  putStr "P: "
+  x <- drawHuman
+  return (x, g)
+
+playerShoot2 :: RandomGen g => g -> GameState -> Player -> IO (Shoot, g)
+playerShoot2 g gameState (Computer mode) = do
+  (x, g1) <- drawMemoryOrRandom mode g (shootSequence gameState)
+  putStrLn ("C: " ++ show x)
+  return (x, g1)
+playerShoot2 g _ (Human _) = do
   putStr "P: "
   x <- drawHuman
   return (x, g)
@@ -123,16 +167,19 @@ main =
 
 gameLoop2 :: Player
           -> Player
-          -> StateT Score IO ()
+          -> StateT GameState IO ()
 gameLoop2 p1 p2 = do
   g <- liftIO getStdGen
-  (fingersP1, g1) <- liftIO $ playerShoot g p1
-  (fingersP2, g2) <- liftIO $ playerShoot g p2
+  gameState <- get
+  (fingersP1, g1) <- liftIO $ playerShoot2 g gameState p1
+  (fingersP2, g2) <- liftIO $ playerShoot2 g gameState p2
   let score' = winner (p1, fingersP1) (p2, fingersP2)
-  newScore <- fmap (mappend score') get
+  let oldScore = score gameState
+  let newScore = mappend oldScore score'
+  let oldSequence = shootSequence gameState
   liftIO $ putStrLn $ winnerShow (p1, fingersP1) (p2, fingersP2)
   liftIO $ putStrLn $ "Score: " ++ show newScore
-  put newScore
+  put (GameState {shootSequence=oldSequence ++ [fingersP1], score=newScore})
   gameLoop2 p1 p2
 
 main2 :: IO ()
@@ -141,7 +188,7 @@ main2 =
       [human, computer] = players
    in do
      putStr $ intro players
-     runStateT (gameLoop2 human computer) mempty
+     runStateT (gameLoop2 human computer) GameState {score=mempty, shootSequence=[]}
      return ()
 
 -- TODO:
