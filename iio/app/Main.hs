3589a7d8c8474f7e61cfd0fcca229157dc4444e5
Arthur Caillau <arthur.caillau@airbnb.com>
2016-12-16 15:31:59 -0800

    [iio] added ini exercise

--- iio/app/Main.hs
+++ iio/app/Main.hs
@@ -1,12 +1,20 @@
 module Main where
 
 import Lib
+import Ini (ini, Config)
 import Control.Monad (mapM)
 import Vigenere (vigenere, unVigenere)
-import System.IO (getContents, hGetChar, hPutStr, hWaitForInput, stderr, stdin, stdout)
+import System.IO (IOMode(ReadMode), getContents, hGetChar, hGetContents, hPutStr, hWaitForInput, openFile, stderr, stdin, stdout)
 import System.Environment (getArgs)
+import Data.List.Split (splitOn)
 import Data.Char (isLetter)
+import qualified Data.Map.Strict as M
 import System.Exit (exitSuccess, ExitCode(ExitFailure, ExitSuccess), exitWith)
+import System.Directory (getDirectoryContents)
+import Text.Trifecta
+import Text.Trifecta.Result
+
+-- Vigenere command Line
 
 data Mode = Decrypt | Encrypt
   deriving Show
@@ -38,3 +46,30 @@ main = do
       Just Decrypt -> hPutStr stdout $ unVigenere key s
       Just Encrypt -> hPutStr stdout $ vigenere key s
   where key = "mykey"
+
+-- INI directory
+
+iniFile :: FilePath -> Bool
+iniFile "." = False
+iniFile ".." = False
+iniFile fp =
+  case splitOn "." fp of
+    []  -> False
+    [x] -> False
+    xs -> last xs == "ini"
+
+configMap :: [FilePath] -> [String] -> M.Map FilePath (Text.Trifecta.Result.Result Ini.Config)
+configMap fps xs = M.fromList $ zip fps parsedContents
+  where parsedContents = map (parseString ini mempty) xs
+
+iniDir :: String
+iniDir = "data"
+
+iniConfig :: IO ()
+iniConfig = do
+  putStrLn "INI directory: "
+  iniDir <- getLine
+  iniFilePaths <- filter iniFile <$> getDirectoryContents iniDir
+  handles <- mapM (\fp -> openFile (iniDir ++ "/" ++ fp) ReadMode) iniFilePaths
+  iniContents <- mapM hGetContents handles
+  print $ configMap iniFilePaths iniContents
