452b21153310e6ea5219a4cf6f9332c92ed733d1
Chouffe <chouffe.caillau@gmail.com>
2017-02-25 23:41:40 -0800

    [nta] redid nta with list monad

--- nta/app/Main.hs
+++ nta/app/Main.hs
@@ -1,6 +1,33 @@
 module Main where
 
-import Lib
+import Lib (check, State(..), StateTransition, AcceptingStates)
+
+table :: StateTransition
+table A A = [A, C]
+table A B = [D]
+table A C = [B]
+table A D = [D]
+table B A = [B]
+table B B = [A, B]
+table B C = [A]
+table B D = [D]
+table C A = [D]
+table C B = [A]
+table C C = [B]
+table C D = [D]
+table D A = [C]
+table D B = [A]
+table D C = [A]
+table D D = [D]
+
+acceptingStates :: AcceptingStates
+acceptingStates = [C, D]
 
 main :: IO ()
-main = output
+main = mapM_ (putStrLn . show . (check table acceptingStates))
+  [ [A, A, B, B]
+  , [A, D, A]
+  , [C, D, B]
+  , [A, A, A]
+  , [ B, B, B, B, B, B]
+  ]
--- nta/src/Lib.hs
+++ nta/src/Lib.hs
@@ -1,42 +1,26 @@
-module Lib where
+module Lib
+  ( check
+  , AcceptingStates
+  , State(..)
+  , StateTransition
+  )
+  where
 
-import qualified Data.Map as Map
-
+import Data.List (nub)
 
 data State = A | B | C | D deriving (Eq, Ord, Show)
 type AcceptingStates = [State]
+type StateTransition = State -> State -> [State]
 
-table :: State -> State -> [State]
-table A A = [A, C]
-table A B = [D]
-table A C = [B]
-table A D = [D]
-table B A = [B]
-table B B = [A, B]
-table B C = [A]
-table B D = [D]
-table C A = [D]
-table C B = [A]
-table C C = [B]
-table C D = [D]
-table D A = [C]
-table D B = [A]
-table D C = [A]
-table D D = [D]
+nextLayer :: StateTransition -> [State] -> [[State]]
+nextLayer _ []         = []
+nextLayer table states = nub $ foldr (\a b -> [x : ys | x <- a, ys <- b]) [[]] leftRightinputs
+  where leftRightinputs = zipWith table states (tail states)
 
-check :: AcceptingStates -> [State] ->Bool
-check [] _ = False  -- No accepting states
-check _ [] = False  -- No state
-check acceptingStates [state] = state `elem` acceptingStates  -- Is state in accepting states?
--- Recursively generate states
-check acceptingStates states@(_:xs) = any (check acceptingStates) $ foldr reducer [[]] $ zipWith table states xs
-  where reducer zs yss = [x : ys | x <- zs, ys <- yss]
-
-output :: IO ()
-output = do
-  mapM_ (putStrLn . show . (check [C, D])) [ [A, A, B, B]
-                                           , [A, D, A]
-                                           , [C, D, B]
-                                           , [A, A, A]
-                                           , [ B, B, B, B, B, B]
-                                           ]
+check :: StateTransition -> AcceptingStates -> [State] -> Bool
+check _ [] _                       = False
+check _ _ []                       = False
+check _ acceptingStates [state]    = state `elem` acceptingStates
+check table acceptingStates states = any (check table acceptingStates) rootStates
+  where n = length states
+        rootStates = iterate (>>= nextLayer table) (return states) !! (n - 1)
